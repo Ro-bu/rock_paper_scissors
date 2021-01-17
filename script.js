57bc62b78988119b14e3b3799ea2ba9313978c8c
Ro-bu <robin.raid1@gmail.com>
2021-01-17 17:30:13 +0200

    reworked thd js logic

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 // MAKE THE COMPUTER CHOOSE
         function computerPlay(){
             let randomNumberForGame = Math.random()*3;
-            console.log(randomNumberForGame);
             if (randomNumberForGame > 2){
                 return "rock";
             } else if (randomNumberForGame > 1){
@@ -10,14 +13,9 @@
                 return "scissors";
             }
         }
-// MAKE THE PLAYER CHOOSE
-        function playerSelection(){
-            let playerSelected = prompt("Rock, Paper, or Scissors?");
-            playerSelected = playerSelected.toLowerCase()
-            return playerSelected;
-        }
-// COMPARE THE CHOICES AND SEE WHO WON THE ROUND
-        function playRound(playerselection, computerselection){
+// PLAY THE ROUND
+        function playRound(playerselection){
+            let computerselection = computerPlay();
             if (playerselection ==="paper" && computerselection === "scissors"){
                 return "computer";
             } else if (playerselection ==="paper" && computerselection ==="rock"){
@@ -35,21 +33,73 @@
             }
 
         }
-// PLAY THE GAME TIL SOMEONE SCORES 5 POINTS
-
-// start game/func on-click/button
-// WHILE LOOP UNTIL HUMAN OR COMP SCORE UNDER 5
-//      round starts on choice/button click
-//      select your weapon
-//      computer selects weapon
-//      evaluate results, winner gets 1 point, tie = 0
-//      return winner
-// congratulate winner
-
-
+// RESET THE SCORE COUNTER ON GAME STARTUP
         function game(){
+            playerScore = 0;
+            document.getElementById("playerScore").innerHTML = playerScore;
+            computerScore = 0;
+            document.getElementById("botScore").innerHTML = computerScore;
+            document.getElementById("symbolTitle").classList.remove("hidden");
+            document.getElementById("symbolCont").classList.remove("hidden");
+            document.getElementById("mainScoreCont").classList.remove("hidden");
+            document.getElementById("buttonCont").classList.add("hidden");
+            document.getElementById("winCont").classList.add("hidden");
+            document.getElementById("loseCont").classList.add("hidden");
 
 
 
         }
+// INCREASE THE SCORE FOR THE WINNER
+        function wonTheRound(winner){
+            if (winner == "player"){
+                playerScore++;
+                document.getElementById("playerScore").innerHTML = playerScore;
+                playerScoreUp();
+            }else if(winner == "computer"){
+                computerScore++;
+                document.getElementById("botScore").innerHTML = computerScore;
+                computerScoreUp();
+            }else{
+                scoreTie();
+        }
+    }
+// CHECK THE SCORE FOR WINNERS
+    function scoreChecker(){
+        if (playerScore > 4){
+            document.getElementById("symbolTitle").classList.add("hidden");
+            document.getElementById("symbolCont").classList.add("hidden");
+            document.getElementById("winCont").classList.remove("hidden");
+            document.getElementById("buttonCont").classList.remove("hidden");
+        }else if (computerScore >4){
+            document.getElementById("symbolTitle").classList.add("hidden");
+            document.getElementById("symbolCont").classList.add("hidden");
+            document.getElementById("loseCont").classList.remove("hidden");
+            document.getElementById("buttonCont").classList.remove("hidden");
+        }
+    }
+// SCORE CHANGE EFFECTS
+function playerScoreUp(){
+    let score = document.getElementById("playerScore");
+    score.classList.add("playerScoreUp");
+    setTimeout(function(){ score.classList.remove("playerScoreUp"); }, 500);
+}
+function computerScoreUp(){
+    let score = document.getElementById("botScore");
+    score.classList.add("botScoreUp");
+    setTimeout(function(){ score.classList.remove("botScoreUp"); }, 500);
+} 
+function scoreTie(){
+    let score1 = document.getElementById("botScore");
+    let score2 = document.getElementById("playerScore");
+    score1.classList.add("tie");
+    score2.classList.add("tie");
+    setTimeout(function(){ score1.classList.remove("tie"); score2.classList.remove("tie"); }, 500);
+} 
+
+// MAIN FUNC THAT GETS CALLED ON PLAY CLICK
+    function playWith(item){
+        let winner = playRound(item);
+        wonTheRound(winner);
+        scoreChecker();
+    }
 
